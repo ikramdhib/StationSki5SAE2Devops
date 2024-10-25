082182acf3a535d77ca7a67a8301c1ae2f64c736
DELL <niheleeroui124@gmail.com>
2024-10-25 13:01:20 +0200

    commit5

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,7 +2,6 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
