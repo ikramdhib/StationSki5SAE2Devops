61214dd123576407c70c68ade481952c28429783
DELL <niheleeroui124@gmail.com>
2024-10-29 13:25:14 +0100

    mainetability3

--- src/main/java/tn/esprit/spring/entities/Course.java
+++ src/main/java/tn/esprit/spring/entities/Course.java
@@ -4,34 +4,50 @@ import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level=AccessLevel.PRIVATE)
 @Entity
 @Builder
 public class Course implements Serializable {
-
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long numCourse;
+
+	@NotNull(message = "Title cannot be null")
+	@Size(min = 1, max = 100, message = "Title must be between 1 and 100 characters")
 	private String title;
-	int level;
+
+	@Positive(message = "Level must be a positive integer")
+	private int level;
+
+	@NotNull(message = "Type of course cannot be null")
 	@Enumerated(EnumType.STRING)
 	TypeCourse typeCourse;
+
+	@Size(max = 255, message = "Description cannot be longer than 255 characters")
 	private String description;
+
+	@NotNull(message = "Support cannot be null")
 	@Enumerated(EnumType.STRING)
 	Support support;
-	Float price;
-	int timeSlot;
+
+	@NotNull(message = "Price cannot be null")
+	@Positive(message = "Price must be a positive number")
+	private Float price;
+
+	@Positive(message = "Time slot must be a positive integer")
+	private int timeSlot;
 
 	@JsonIgnore
-	@OneToMany(mappedBy= "course")
+	@OneToMany(mappedBy = "course")
 	private Set<Registration> registrations;
-
 }
--- src/main/java/tn/esprit/spring/entities/CourseDTO.java
+++ src/main/java/tn/esprit/spring/entities/CourseDTO.java
@@ -13,6 +13,8 @@ public class CourseDTO {
     @NotNull(message = "Title cannot be null")
     @Size(min = 1, max = 100, message = "Title must be between 1 and 100 characters")
     private String title;
+
+    @NotNull(message = "Level cannot be null")
     @Positive(message = "Level must be a positive integer")
     private int level;
 
@@ -22,10 +24,14 @@ public class CourseDTO {
     @Size(max = 255, message = "Description cannot be longer than 255 characters")
     private String description;
 
+    @NotNull(message = "Support cannot be null")
     private Support support;
+
     @NotNull(message = "Price cannot be null")
     @Positive(message = "Price must be a positive number")
     private Float price;
+
+    @NotNull(message = "Time slot cannot be null")
     @Positive(message = "Time slot must be a positive integer")
     private int timeSlot;
 }
