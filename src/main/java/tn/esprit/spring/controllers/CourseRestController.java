04fd349ac3622f976fa18d73ea719367bdf0ac39
DELL <niheleeroui124@gmail.com>
2024-10-29 17:48:02 +0100

    correction

--- src/main/java/tn/esprit/spring/controllers/CourseRestController.java
+++ src/main/java/tn/esprit/spring/controllers/CourseRestController.java
@@ -17,49 +17,40 @@ import java.util.List;
 @RequestMapping("/course")
 @RequiredArgsConstructor
 public class CourseRestController {
-
-    private static final String ADD_COURSE_URL = "/add";
-    private static final String GET_ALL_COURSES_URL = "/all";
-    private static final String UPDATE_COURSE_URL = "/update";
-    private static final String GET_COURSE_BY_ID_URL = "/get/{id-course}";
-    private static final String DELETE_COURSE_URL = "/deleteCourse/{numCourse}";
-    private static final String SEARCH_COURSES_URL = "/search";
-    private static final String RECOMMEND_COURSES_URL = "/recommend";
-
+    
     private final ICourseServices courseServices;
 
-
     @Operation(description = "Add Course")
-    @PostMapping(ADD_COURSE_URL)
+    @PostMapping("/add")
     public Course addCourse(@RequestBody CourseDTO course) {
         return courseServices.addCourse(course);
     }
     @Operation(description = "Retrieve all Courses")
-    @GetMapping(GET_ALL_COURSES_URL)
+    @GetMapping("/all")
     public List<Course> getAllCourses() {
         return courseServices.retrieveAllCourses();
     }
 
     @Operation(description = "Update Course")
-    @PutMapping(UPDATE_COURSE_URL)
+    @PutMapping("/update")
     public Course updateCourse(@RequestBody CourseDTO course) {
         return courseServices.updateCourse(course);
     }
 
     @Operation(description = "Retrieve Course by Id")
-    @GetMapping(GET_COURSE_BY_ID_URL)
+    @GetMapping("/get/{id-course}")
     public Course getById(@PathVariable("id-course") Long numCourse) {
         return courseServices.retrieveCourse(numCourse);
     }
 
     @Operation(description = "Delete Course by Id")
-    @DeleteMapping(DELETE_COURSE_URL)
+    @DeleteMapping("/deleteCourse/{numCourse}")
     public void deleteCourse(@PathVariable Long numCourse) {
         courseServices.deleteCourse(numCourse);
     }
 
     @Operation(description = "Search Courses based on criteria")
-    @GetMapping(SEARCH_COURSES_URL)
+    @GetMapping("/search")
     public List<Course> searchCourses(
             @RequestParam(required = false) Integer level,
             @RequestParam(required = false) TypeCourse typeCourse,
@@ -69,7 +60,7 @@ public class CourseRestController {
     }
 
     @Operation(description = "Recommend Courses based on type")
-    @GetMapping(RECOMMEND_COURSES_URL)
+    @GetMapping("/recommend")
     public ResponseEntity<List<Course>> recommendCourses(@RequestParam TypeCourse typeCourse) {
         List<Course> recommendedCourses = courseServices.recommendCourses(typeCourse);
         return new ResponseEntity<>(recommendedCourses, HttpStatus.OK);
