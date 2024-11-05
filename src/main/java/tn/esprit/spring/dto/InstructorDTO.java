fa5fa7a384b3846abf791aa9b91119a30f7b12c4
DELL <niheleeroui124@gmail.com>
2024-11-05 17:08:35 +0100

    test2

--- src/main/java/tn/esprit/spring/dto/InstructorDTO.java
+++ src/main/java/tn/esprit/spring/dto/InstructorDTO.java
@@ -1,14 +1,6 @@
 package tn.esprit.spring.dto;
-
 import lombok.*;
-import lombok.experimental.FieldDefaults;
 import tn.esprit.spring.entities.Course;
-import tn.esprit.spring.entities.Instructor;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.Set;
 
