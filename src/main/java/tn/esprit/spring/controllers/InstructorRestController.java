fa6b3f1968ecd9b23d1277ce5108a4e0407a650a
DELL <niheleeroui124@gmail.com>
2024-11-05 18:56:09 +0100

    Updateeee

--- src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
+++ src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
@@ -18,62 +18,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InstructorRestController {
 
-    private static final String ADD_INSTRUCTOR_URL = "/add";
-    private static final String ADD_ASSIGN_INSTRUCTOR_URL = "/addAndAssignToCourse/{numCourse}";
-    private static final String GET_ALL_INSTRUCTORS_URL = "/all";
-    private static final String UPDATE_INSTRUCTOR_URL = "/update";
-    private static final String GET_INSTRUCTOR_BY_ID_URL = "/get/{id-instructor}";
-
     private final IInstructorServices instructorServices;
 
-    // Méthode pour convertir InstructorDTO en Instructor
-    private Instructor convertToInstructor(InstructorDTO instructorDTO) {
-        return Instructor.builder()
-                .firstName(instructorDTO.getFirstName())
-                .lastName(instructorDTO.getLastName())
-                .dateOfHire(instructorDTO.getDateOfHire())
-                .build();
-    }
-
     @Operation(description = "Add Instructor")
-    @PostMapping(ADD_INSTRUCTOR_URL)
-    public ResponseEntity<Instructor> addInstructor(@RequestBody InstructorDTO instructorDTO) {
-        Instructor instructor = convertToInstructor(instructorDTO);
-        Instructor addedInstructor = instructorServices.addInstructor(instructor);
-        return new ResponseEntity<>(addedInstructor, HttpStatus.CREATED);
+    @PostMapping(value = "/add")
+    public Instructor addInstructor(@RequestBody Instructor instructorDTO){
+        return  instructorServices.addInstructor(instructorDTO);
     }
-
     @Operation(description = "Add Instructor and Assign To Course")
-    @PutMapping(ADD_ASSIGN_INSTRUCTOR_URL)
-    public ResponseEntity<Instructor> addAndAssignToInstructor(@RequestBody InstructorDTO instructorDTO,
-                                                               @PathVariable("numCourse") Long numCourse) {
-        Instructor instructor = convertToInstructor(instructorDTO);
-        Instructor addedInstructor = instructorServices.addInstructorAndAssignToCourse(instructor, numCourse);
-        return new ResponseEntity<>(addedInstructor, HttpStatus.OK);
+    @PutMapping(value = "/addAndAssignToCourse/{numCourse}")
+    public Instructor addAndAssignToInstructor(@RequestBody Instructor instructor, @PathVariable("numCourse")Long numCourse){
+        return  instructorServices.addInstructorAndAssignToCourse(instructor,numCourse);
     }
-
     @Operation(description = "Retrieve all Instructors")
-    @GetMapping(GET_ALL_INSTRUCTORS_URL)
-    public ResponseEntity<List<Instructor>> getAllInstructors() {
-        List<Instructor> instructors = instructorServices.retrieveAllInstructors();
-        return new ResponseEntity<>(instructors, HttpStatus.OK);
+    @GetMapping(value = "/all")
+    public List<Instructor> getAllInstructors(){
+        return instructorServices.retrieveAllInstructors();
     }
 
-    @Operation(description = "Update Instructor")
-    @PutMapping(UPDATE_INSTRUCTOR_URL)
-    public ResponseEntity<Instructor> updateInstructor(@RequestBody InstructorDTO instructorDTO) {
-        Instructor instructor = convertToInstructor(instructorDTO);
-        Instructor updatedInstructor = instructorServices.updateInstructor(instructor);
-        return new ResponseEntity<>(updatedInstructor, HttpStatus.OK);
+    @Operation(description = "Update Instructor ")
+    @PutMapping(value = "/update")
+    public Instructor updateInstructor(@RequestBody Instructor instructor){
+        return  instructorServices.updateInstructor(instructor);
     }
 
     @Operation(description = "Retrieve Instructor by Id")
-    @GetMapping(GET_INSTRUCTOR_BY_ID_URL)
-    public ResponseEntity<Instructor> getById(@PathVariable("id-instructor") Long numInstructor) {
-        Instructor instructor = instructorServices.retrieveInstructor(numInstructor);
-        if (instructor == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(instructor, HttpStatus.OK);
+    @GetMapping(value = "/get/{id-instructor}")
+    public Instructor getById(@PathVariable("id-instructor") Long numInstructor){
+        return instructorServices.retrieveInstructor(numInstructor);
     }
 }
--- src/main/java/tn/esprit/spring/controllers/SubscriptionRestController.java
+++ src/main/java/tn/esprit/spring/controllers/SubscriptionRestController.java
@@ -21,61 +21,33 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class SubscriptionRestController {
 
-    private static final String ADD_SUBSCRIPTION_URL = "/add";
-    private static final String GET_SUBSCRIPTION_BY_ID_URL = "/get/{id-subscription}";
-    private static final String GET_SUBSCRIPTIONS_BY_TYPE_URL = "/all/{typeSub}";
-    private static final String UPDATE_SUBSCRIPTION_URL = "/update";
-    private static final String GET_SUBSCRIPTIONS_BY_DATES_URL = "/all/{date1}/{date2}";
-
     private final ISubscriptionServices subscriptionServices;
 
-    // Méthode privée pour convertir SubscriptionDTO en Subscription
-    private Subscription convertToSubscription(SubscriptionDTO subscriptionDTO) {
-        return Subscription.builder()
-                .startDate(subscriptionDTO.getStartDate())
-                .endDate(subscriptionDTO.getEndDate())
-                .price(subscriptionDTO.getPrice())
-                .build();
+    @Operation(description = "Add Subscription ")
+    @PostMapping(value = "/add")
+    public Subscription addSubscription(@RequestBody Subscription subscriptionTDO){
+        return  subscriptionServices.addSubscription(subscriptionTDO);
     }
-
-    @Operation(description = "Add Subscription")
-    @PostMapping(ADD_SUBSCRIPTION_URL)
-    public ResponseEntity<Subscription> addSubscription(@RequestBody SubscriptionDTO subscriptionDTO) {
-        Subscription subscription = convertToSubscription(subscriptionDTO);
-        Subscription addedSubscription = subscriptionServices.addSubscription(subscription);
-        return new ResponseEntity<>(addedSubscription, HttpStatus.CREATED);
-    }
-
     @Operation(description = "Retrieve Subscription by Id")
-    @GetMapping(GET_SUBSCRIPTION_BY_ID_URL)
-    public ResponseEntity<Subscription> getById(@PathVariable("id-subscription") Long numSubscription) {
-        Subscription subscription = subscriptionServices.retrieveSubscriptionById(numSubscription);
-        if (subscription == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(subscription, HttpStatus.OK);
+    @GetMapping(value = "/get/{id-subscription}")
+    public Subscription getById(@PathVariable("id-subscription") Long numSubscription){
+        return subscriptionServices.retrieveSubscriptionById(numSubscription);
     }
 
     @Operation(description = "Retrieve Subscriptions by Type")
-    @GetMapping(GET_SUBSCRIPTIONS_BY_TYPE_URL)
-    public ResponseEntity<Set<Subscription>> getSubscriptionsByType(@PathVariable("typeSub") TypeSubscription typeSubscription) {
-        Set<Subscription> subscriptions = subscriptionServices.getSubscriptionByType(typeSubscription);
-        return new ResponseEntity<>(subscriptions, HttpStatus.OK);
+    @GetMapping(value = "/all/{typeSub}")
+    public Set<Subscription> getSubscriptionsByType(@PathVariable("typeSub") TypeSubscription typeSubscription){
+        return subscriptionServices.getSubscriptionByType(typeSubscription);
     }
-
-    @Operation(description = "Update Subscription")
-    @PutMapping(UPDATE_SUBSCRIPTION_URL)
-    public ResponseEntity<Subscription> updateSubscription(@RequestBody SubscriptionDTO subscriptionDTO) {
-        Subscription subscription = convertToSubscription(subscriptionDTO);
-        Subscription updatedSubscription = subscriptionServices.updateSubscription(subscription);
-        return new ResponseEntity<>(updatedSubscription, HttpStatus.OK);
+    @Operation(description = "Update Subscription ")
+    @PutMapping(value = "/update")
+    public Subscription updateSubscription(@RequestBody Subscription subscriptionTDO){
+        return  subscriptionServices.updateSubscription(subscriptionTDO);
     }
-
     @Operation(description = "Retrieve Subscriptions created between two dates")
-    @GetMapping(GET_SUBSCRIPTIONS_BY_DATES_URL)
-    public ResponseEntity<List<Subscription>> getSubscriptionsByDates(@PathVariable("date1") LocalDate startDate,
-                                                                      @PathVariable("date2") LocalDate endDate) {
-        List<Subscription> subscriptions = subscriptionServices.retrieveSubscriptionsByDates(startDate, endDate);
-        return new ResponseEntity<>(subscriptions, HttpStatus.OK);
+    @GetMapping(value = "/all/{date1}/{date2}")
+    public List<Subscription> getSubscriptionsByDates(@PathVariable("date1") LocalDate startDate,
+                                                      @PathVariable("date2") LocalDate endDate){
+        return subscriptionServices.retrieveSubscriptionsByDates(startDate, endDate);
     }
 }
