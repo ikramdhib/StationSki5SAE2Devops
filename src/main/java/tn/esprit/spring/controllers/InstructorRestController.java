ec03875607d10c9b403e60a7acb3c51b4d70c840
DELL <niheleeroui124@gmail.com>
2024-11-05 20:04:47 +0100

    test5

--- src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
+++ src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.dto.InstructorDTO;
-import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Instructor;
 import tn.esprit.spring.entities.InstructorMapper;
 import tn.esprit.spring.services.IInstructorServices;
